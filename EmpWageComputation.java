f5bf990f7a6c846b4b43d371b0250fcc5f7bc21c
[decoderNikhil] <[Nikhilmit777@gmail.com]>
2021-11-03 12:58:05 +0530

    EmpWage Using SWITCH

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -1,10 +1,40 @@
    public class EmpWageComputation{
 
-            public static void main(String[] args){
+          public static  final int Parttime = 1;
 
-               System.out.println("WELCOME TO THE EMPLOYEE WAGE COMPUTATION PROGRAM");
+         public static   final int Fulltime = 2;
 
+    public static  final int EmpRateprHr = 20;
 
+                   public static void main(String[] args) {
+
+                             int EmpHrs = 0; 
+
+                             int EmpWage = 0; 
+
+                             
+                int empCheck = (int) Math.floor(Math.random()* 10) % 3;
+
+                     switch (empCheck) {
+ 
+                    case Parttime:
+                                      EmpHrs = 4;
+
+                 break;
+
+                    case Fulltime:
+                                          EmpHrs = 8;
+                 break;
+               
+                   default:
+
+                       EmpHrs = 0;
+
+                      }
+
+                     EmpWage = EmpHrs * EmpRateprHr;
+                    
+                     System.out.println("Emp Wage: " + EmpWage);
           }
  
 }
