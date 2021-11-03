ed286f2356b353efa5c0f9a6724c31b1a4622d97
[decoderNikhil] <[Nikhilmit777@gmail.com]>
2021-11-03 11:26:31 +0530

    WELCOME MESSAGE

---
+++ EmpWageComputation.java
@@ -0,0 +1,10 @@
+   public class EmpWageComputation{
+
+            public static void main(String[] args){
+
+               System.out.println("WELCOME TO THE EMPLOYEE WAGE COMPUTATION PROGRAM");
+
+
+          }
+ 
+}
