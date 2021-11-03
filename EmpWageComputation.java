66a0ef03b845200041941ad3370cc6af5326c67f
[decoderNikhil] <[Nikhilmit777@gmail.com]>
2021-11-03 13:19:08 +0530

    Calculataed Wages For a month

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -1,10 +1,58 @@
    public class EmpWageComputation{
 
-            public static void main(String[] args){
+        
 
-               System.out.println("WELCOME TO THE EMPLOYEE WAGE COMPUTATION PROGRAM");
+                       public static final int Parttime= 1;
+
+                       public static final int Fulltime = 2;  
+                       
+                       public static final int EmpRateprHr = 20;
+
+                       public static final int NumOfwrkDays = 2; 
+       
+
+             
+                                            public static void main(String[] args) {
+
+                                                     int empHrs= 0, empWage = 0, totalEmpWage = 0;
 
 
-          }
+
+                                                  for (int day = 0; day< NumOfwrkDays; day++) { 
+
+                                                      
+                                                      int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+
+
+                                                    switch (empCheck) { 
+                   
+                                                       case Parttime:
+
+                                                                   empHrs = 4;
+
+                                                         break;
+
+                                                     case Fulltime: 
+                                                                     
+                                                                     empHrs= 8;
+
+                                                          break;
+   
+                                                           default:
+
+                                                                 empHrs= 0;
+
+                                                }
+
+                                                          empWage = empHrs * EmpRateprHr;
+
+                                                         totalEmpWage += empWage;
+                   
+                                                           System.out.println("Emp Wage: " + empWage);
+                                                        } 
+
+                                                           System.out.println("Total Emp Wage;" + totalEmpWage);
+
+                                            }
  
-}
+                      }
