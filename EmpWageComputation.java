8a342fc67361df8165e8de95d23bf8a9e6f31dc1
[decoderNikhil] <[Nikhilmit777@gmail.com]>
2021-11-03 12:38:06 +0530

    PART TIME ADDED

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -1,10 +1,39 @@
    public class EmpWageComputation{
 
-            public static void main(String[] args){
+            public static void main(String[] args) {
+  
 
-               System.out.println("WELCOME TO THE EMPLOYEE WAGE COMPUTATION PROGRAM");
+             int Parttime = 1;
+
+            int Fulltime = 2;
+
+            int EmpRateprHr = 20; 
+
+                   int EmpHrs = 0;
+
+                    int EmpWage = 0;
 
 
-          }
- 
-}
+
+                          double empCheck = Math.floor(Math.random()* 10) % 3;
+
+                             if (empCheck == Parttime)
+
+                                EmpHrs = 4; 
+
+                                   else if (empCheck == Fulltime)
+
+                                      EmpHrs = 8;
+
+                                           else
+                                                 EmpHrs =0 ;
+                                        EmpWage = EmpHrs * EmpRateprHr;
+
+                                            System.out.println("Emp Wage: "+EmpWage);
+
+
+
+                }
+
+
+            }
