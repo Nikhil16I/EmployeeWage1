3a5702c19c86ec04daaebefac9cbc27d089afc11
Nikhil Patil16 <92767519+Nikhil16I@users.noreply.github.com>
2021-11-17 10:48:42 +0530

    Saving Total Wages For Each Company

--- MainEmpwage.java
+++ MainEmpwage.java
@@ -5,16 +5,16 @@ public class MainEmpwage{
 		System.out.println("Welcome to Employee Wage Computation");
 	
 EmpWageComputation1 TCS = new EmpWageComputation1 ("TCS",25,80,35);
-    TCS.ComputeEmployeewage(null, 0, 0, 0);	
+    TCS.ComputeEmployeewage;	
              System.out.println(TCS);	
 	
 	
 EmpWageComputation1 CapeGemini = new EmpWageComputation1("CapeGemini",30,90,45);
-    CapeGemini.ComputeEmployeewage(null, 0, 0, 0);
+    CapeGemini.ComputeEmployeewage;
            System.out.println(CapeGemini );    
               
 EmpWageComputation1  HCL = new  EmpWageComputation1("HCL",35,85,30);
-     HCL.ComputeEmployeewage(null, 0, 0, 0);
+     HCL.ComputeEmployeewage;
               System.out.println(HCL);
   }
 }
